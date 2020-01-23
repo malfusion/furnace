13e49c8b8759cfb81665d5ac5b34b8da348e0287
malfusion <judenaveenraj@gmail.com>
2020-01-23 17:34:06 -0500

    Figured out the right way to serialize processed numpy image for kafka

--- FurnaceUploader.py
+++ FurnaceUploader.py
@@ -6,11 +6,14 @@ from CVUtils import resizeFrame, showFrame
 import datetime
 import json
 import cv2
+import numpy as np
+import base64
+
 
 class FurnaceUploader():
     def __init__(self, configFilePath="./config.json"):
         self.config = self.readConfig(configFilePath)
-        self.kafkaProducer = self.initProducer()
+        self.initProducer()
         
 
     def readConfig(self, filepath):
@@ -26,6 +29,14 @@ class FurnaceUploader():
         else:
             raise Exception("Cannot find Kafka Producer configuration.")
 
+    def producerSend(self, data):
+        self.kafkaProducer.poll(1.0)
+        # b = np.fromstring((json.loads(a))["frame"], 'uint8')
+        # p.produce('frames', json.dumps(frameRecord).encode('utf-8'), callback=delivery_report)
+        encodedData = json.dumps(data)
+        print("Message size", len(encodedData))
+        self.kafkaProducer.produce('frames', encodedData, callback=kafkaDeliveryReport)
+
     def uploadFile(self, filepath, startTime):
         frameExtractor = FrameExtractor(filepath, 1)
         batcher = DataBatcher(keyFunc=batchKeyFun, valFunc=batchValueFun)
@@ -34,6 +45,18 @@ class FurnaceUploader():
             # showFrame(resizedFrame)
             dt = datetime.timedelta(seconds=sec)
             frameTime = startTime + dt
+
+            # START SHORTCIRCUIT BATCHING
+            print(resizedFrame.shape)
+            frameRecord = {
+                "camera": 'camera1',
+                "timestamp": str(frameTime),
+                "timetype": "second",
+                "frame": base64.b64encode(resizedFrame.tobytes()).decode("utf-8")    # While decoding, do numpy.array(json.loads()) 
+            }
+            self.producerSend(frameRecord)
+
+            # END SHORTCIRCUIT BATCHING
             batcher.addData((frameTime, resizedFrame))
             for batch in batcher.getBatches():
                 # Process batch here
@@ -44,7 +67,9 @@ class FurnaceUploader():
             # Process last batch here
             for key, val in batch.items():
                 print("Process batch with key:", key, "and number of frames:", len(val))
+        self.kafkaProducer.flush()
         
+
 def batchKeyFun(data):
     frameTime = data[0]
     return str((frameTime) - datetime.timedelta(seconds=(frameTime).second))
